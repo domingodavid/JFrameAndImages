b56fb42fcd2986784b37f8bdefac7fc8294dd0d2
Dom <Dom@192.168.1.218>
2021-04-21 18:13:10 -0700

    initial commit

---
+++ ImageBasedProjects/src/Duck.java
@@ -0,0 +1,10 @@
+
+public class Duck extends Sprite {
+
+	public Duck(String fileName) {
+		super(fileName);
+		width 	= 100;
+		height 	= 100;
+	}
+
+}
---
+++ ImageBasedProjects/src/Sprite.java
@@ -0,0 +1,95 @@
+
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public abstract class Sprite{
+	protected Image img; // image for sprite
+	protected Image alternate;
+	protected int x, y; // location
+	protected int width; 
+	protected int height;
+	
+	public Sprite(String fileName) {
+		// assignment statements for attributes
+		img = getImage(fileName); //load the image
+		init(x, y); //initialize the x,y position of the image
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	// gets image and process it
+	public void update() {
+		tx.setToTranslation(x, y);
+	}
+
+	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
+
+	// draw the affine transform
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		update();
+		g2.drawImage(img, tx, null);
+		
+	}
+
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(1, 1);
+	}
+
+	// converts image to make it drawable in paint
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Sprite.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+	/* Helper function for collision detection later */
+	public Rectangle getRect() {
+		Rectangle temp = new Rectangle(x,y,width,height);
+		return temp;
+	}
+
+}
